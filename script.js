aeff2c540258a24752c7dd7063a8f72ab833c244
Jesse Granier <jesse.granier@lapiscine.pro>
2024-02-16 13:57:12 +0100

    First Training

---
+++ script.js
@@ -0,0 +1,33 @@
+/* ####### STRUCTURE DU JEU ####### */
+// const listeMots = ["Cachalot", "Pétunia", "Serviette"];
+// let score = 0;
+// let enterWord = prompt("Entrez le mot : " + listeMots[0]);
+
+// if (enterWord === listeMots[0]) {
+//     console.log("Le mot est correct")
+//     score += 1;
+//     console.log("Votre score est maintenant de : " + score);
+// } else {
+//     console.log("Le mot est incorrect")
+// }
+
+
+// enterWord = prompt("Entrez le mot : " + listeMots[1]);
+// if (enterWord === listeMots[1]) {
+//     console.log("Le mot est correct")
+//     score += 1;
+//     console.log("Votre score est maintenant de : " + score);
+// } else {
+//     console.log("Le mot est incorrect")
+//     console.log("Votre score est toujours de : " + score)
+// }
+
+// enterWord = prompt("Entrez le mot : " + listeMots[2]);
+// if (enterWord === listeMots[2]) {
+//     console.log("Le mot est correct")
+//     score += 1;
+//     console.log("Votre score est maintenant de : " + score);
+// } else {
+//     console.log("Le mot est incorrect")
+//     console.log("Votre score est toujours de : " + score)
+// }
